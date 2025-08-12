7a586aacf9d0ae1369c295552a3be531ff99cdb6
Aditya Raj <aditya.rajx77x@gmail.com>
2025-08-12 21:44:05 +0530

    Time: 37 ms (81.17%) | Memory: 55.2 MB (6.12%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,5 +4,7 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    return arr.map(fn);
+    let temp = [];
+    for(let i=0;i<arr.length;i++) temp[i] = fn(arr[i], i);
+    return temp;
 };
