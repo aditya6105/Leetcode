13e40f33b7bc24ac07235f89fab99f6ce26f1238
Aditya Raj <aditya.rajx77x@gmail.com>
2025-08-12 22:02:13 +0530

    Time: 43 ms (48.60%) | Memory: 54 MB (27.52%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,5 +4,9 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    return arr.filter(fn);
+    let a = [];
+    for(let i=0;i<arr.length;i++){
+        if(fn(arr[i], i)) a.push(arr[i]);
+    }
+    return a;
 };
