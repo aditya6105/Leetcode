3ca1ab34677291f49ff145ce0bb1837f6b8b11cf
Aditya Raj <aditya.rajx77x@gmail.com>
2025-08-18 20:42:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (53.67%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,19 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low, mid, high = 0, 0, len(nums) - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                # swap nums[low] and nums[mid]
+                nums[low], nums[mid] = nums[mid], nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            else:  # nums[mid] == 2
+                # swap nums[mid] and nums[high]
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
