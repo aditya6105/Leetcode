81a54048eb170f7cdf90574014e117c976253c8c
Aditya Raj <aditya.rajx77x@gmail.com>
2025-08-20 17:56:56 +0530

    Time: 581 ms (45.10%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -5,6 +5,8 @@
 
 select c.customer_id
 from Customer as c
-join Product as p 
 group by customer_id
-having count(distinct c.product_key) = count(distinct p.product_key)
+having count(distinct c.product_key) = (
+    select count(product_key)
+    from product
+)
