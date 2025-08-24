e01bc5fa7f451d2c16e15fa86289f64f8561d203
Aditya Raj <aditya.rajx77x@gmail.com>
2025-08-24 16:36:19 +0530

    Time: 280 ms (92.10%) | Memory: 0B (100.00%) - LeetSync

---
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,11 @@
+select id, count(id) as num
+from (
+    select requester_id as id from requestaccepted
+
+    union all
+
+    select accepter_id from requestaccepted
+) as friend_count
+group by id
+order by num desc
+limit 1
