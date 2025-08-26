b51eab2442c19009b0b90e28c2d8a2941cc47937
Aditya Raj <aditya.rajx77x@gmail.com>
2025-08-26 12:25:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (32.31%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -3,13 +3,13 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> v;
         for(int i=1;i<=numRows;i++){
+            vector<int> vec = {1};
             int a = 1;
-            vector<int> temp = {1};
             for(int j=1;j<i;j++){
                 a = a * (i - j) / j;
-                temp.push_back(a);
+                vec.push_back(a);
             }
-            v.push_back(temp);
+            v.push_back(vec);
         }
         return v;
     }
