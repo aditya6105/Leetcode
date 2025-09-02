1b97ba68cd9f87a4457ac977942a2260f093e334
Aditya Raj <aditya.rajx77x@gmail.com>
2025-09-02 14:17:22 +0530

    Time: 11 ms (54.81%) | Memory: 32.6 MB (39.26%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        int c = 0;
-        int n = points.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i == j) continue;
+        int count=0;
+        int n=points.size();
 
-                // Treat points[i] as A(upper-left), points[j] as B(bottom-right)
-                if(points[i][0] <= points[j][0] && points[i][1] >= points[j][1]){ 
-                    bool flag = true;
-                    for(int k = 0; k < n; k++){
-                        if(k == i || k == j) continue;
+        for(int i=0; i<n; i++) {
+            int x1=points[i][0],y1=points[i][1];
+            for(int j=0; j<n; j++) {
+                if(i==j) continue;
+                int x2=points[j][0],y2=points[j][1];
+                if(x1>x2 || y1<y2) continue;
 
-                        // check if k lies inside or on the rectangle with corners A (i) and B (j)
-                        if(points[i][0] <= points[k][0] && points[k][0] <= points[j][0] &&
-                           points[j][1] <= points[k][1] && points[k][1] <= points[i][1]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if(flag) c++;
+                bool flag=true;
+                for(int k=0; k<n; k++) {
+                    if(k==i || k==j) continue;
+                    int x=points[k][0],y=points[k][1];
+
+                    if(x>=x1 && x<=x2 && y<=y1 && y>=y2) {
+                        flag=false;
+                        break;
+                    }                        
                 }
+                if(flag==true) count++;
             }
         }
-        return c;
+
+        return count;
     }
 };
