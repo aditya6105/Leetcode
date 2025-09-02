c3e7d3c562b184605be48626812c733e08b75ae1
Aditya Raj <aditya.rajx77x@gmail.com>
2025-09-02 14:01:46 +0530

    Time: 13 ms (45.19%) | Memory: 32.5 MB (88.89%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfPairs(vector<vector<int>>& points) {
+        int c = 0;
+        int n = points.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i == j) continue;
+                //points[i][0] -> x && points[i][1] -> y
+                if(points[i][0] <= points[j][0] && points[i][1] >= points[j][1]){ 
+                    bool flag = true;
+                    for(int k = 0; k < n; k++){
+                        if(k == i || k == j) continue;
+
+                        // check if k lies inside or on the rectangle with corners A (i) and B (j)
+                        if(points[i][0] <= points[k][0] && points[k][0] <= points[j][0] &&
+                           points[j][1] <= points[k][1] && points[k][1] <= points[i][1]) {
+                            flag = false;
+                            break;
+                        }
+                    }
+                    if(flag) c++;
+                }
+            }
+        }
+        return c;
+    }
+};
