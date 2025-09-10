355e665afcda923e684478dfbfe42e0e6cc083e4
Aditya Raj <aditya.rajx77x@gmail.com>
2025-09-10 17:32:29 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.62%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        return pow(x, n);
+        long temp = n;
+        if(n < 0){
+            x = 1/x;
+            temp = -temp;
+        }
+        
+        double a = 1.0;
+        while(temp){
+            if(temp % 2 == 1) a *= x;
+            x *= x;
+            temp /= 2;
+        }
+        return a;
     }
 };
