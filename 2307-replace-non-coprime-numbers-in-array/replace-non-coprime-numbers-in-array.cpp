277bca2b6abbb5c755290b5fa9dafd7d9d76e8dc
Aditya Raj <aditya.rajx77x@gmail.com>
2025-09-16 23:03:15 +0530

    Time: 2192 ms (5.09%) | Memory: 121 MB (99.07%) - LeetSync

---
+++ 2307-replace-non-coprime-numbers-in-array/replace-non-coprime-numbers-in-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        for (int i = 0; i < (int)nums.size() - 1; ) {
+            int g = std::gcd(nums[i], nums[i+1]);
+            if (g > 1) {
+                // compute LCM safely
+                long long l = (1LL * nums[i] * nums[i+1]) / g;
+                nums[i] = (int)l;
+                nums.erase(nums.begin() + i + 1); // remove the right neighbor
+                if (i > 0) --i;  // step back to recheck previous adjacency
+            } else {
+                ++i; // advance only when no merge happened
+            }
+        }
+        return nums;
+    }
+};
