b9ea7280e0601b034ad46f8bb920046b510fd3ea
Aditya Raj <aditya.rajx77x@gmail.com>
2025-10-10 23:01:55 +0530

    Time: 1 ms (23.25%) | Memory: 8.6 MB (50.69%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if(x == 0 || x == 1) return x;
-        long long i = 1;
-        while(i * i <= x) i++;
-        return i-1;
+        if (x == 0 || x == 1) return x;
+        int low = 1;
+        int high = x / 2;
+        while(low <= high){
+            long long mid = low + (high - low) / 2;
+            if(1ll * mid * mid == x) return mid;
+            // cout<<mid<<endl;
+            else if(1ll * mid * mid < x) low = mid + 1;
+            else if(1ll * mid * mid > x) high = mid - 1;
+        }
+        return high;
     }
 };
