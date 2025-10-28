60df629191c111a1e378db992772188f0b3b1eb9
Aditya Raj <aditya.rajx77x@gmail.com>
2025-10-29 02:34:12 +0530

    Time: 12 ms (25.71%) | Memory: 21.7 MB (100.00%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,48 +1,24 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
-        int count = 0;
-        int nonZeros = 0;
         int n = nums.size();
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                nonZeros++;
-            }
-        }
-
+        int ans = 0;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int leftSum = 0;
+        int rightSum = sum;
         for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
-                if (isValid(nums, nonZeros, i, -1)) {
-                    count++;
+                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
+                    ans++;
                 }
-                if (isValid(nums, nonZeros, i, 1)) {
-                    count++;
+                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
+                    ans++;
                 }
+            } else {
+                leftSum += nums[i];
+                rightSum -= nums[i];
             }
         }
-
-        return count;
-    }
-
-private:
-    bool isValid(const vector<int>& nums, int nonZeros, int start,
-                 int direction) {
-        int n = nums.size();
-        vector<int> temp(nums);
-        int curr = start;
-
-        while (nonZeros > 0 && curr >= 0 && curr < n) {
-            if (temp[curr] > 0) {
-                temp[curr]--;
-                direction *= -1;
-                if (temp[curr] == 0) {
-                    nonZeros--;
-                }
-            }
-            curr += direction;
-        }
-
-        return nonZeros == 0;
+        return ans;
     }
 };
