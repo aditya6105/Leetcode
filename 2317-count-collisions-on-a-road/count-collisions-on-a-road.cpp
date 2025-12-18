92a8138f7c14b0bbeb0767833a47835a4e596cb8
Aditya Raj <aditya.rajx77x@gmail.com>
2025-12-18 12:16:08 +0530

    Time: 10 ms (55.09%) | Memory: 19.3 MB (44.01%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    int countCollisions(string d) {
-        int n = d.size();
-        int c = 0;
-
-        for (int i = 1; i < n; i++) {
-            while (i > 0) {
-                // Case 1: R meets L => +2, both stop
-                if (d[i - 1] == 'R' && d[i] == 'L') {
-                    c += 2;
-                    d[i - 1] = 'S';
-                    d[i] = 'S';
-                    i--; // re-check with earlier cars
-                }
-                // Case 2: R meets S => +1, R becomes S
-                else if (d[i - 1] == 'R' && d[i] == 'S') {
-                    c += 1;
-                    d[i - 1] = 'S';
-                    i--;
-                }
-                // Case 3: S meets L => +1, L becomes S
-                else if (d[i - 1] == 'S' && d[i] == 'L') {
-                    c += 1;
-                    d[i] = 'S';
-                    break;
-                } 
-                else {
-                    break; // no collision
-                }
-            }
+    int countCollisions(string dir) {
+        int i = 0;
+        int j = dir.size() - 1;
+        while(i < dir.size() && dir[i] == 'L') i++;
+        while(j < dir.size() && dir[j] == 'R') j--;
+        int cntL = 0;
+        while(i < dir.size()){
+            if(dir[i] == 'L') cntL++;
+            i++;
         }
-
-        return c;
+        int cntR = 0;
+        while(j >= 0){
+            if(dir[j] == 'R') cntR++;
+            j--;
+        }
+        return cntL + cntR;
     }
 };
