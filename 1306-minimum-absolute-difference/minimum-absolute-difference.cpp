f925060256c8654709b1cf8869e11687051ffcea
Aditya Raj <aditya.rajx77x@gmail.com>
2026-01-26 07:18:22 +0530

    Time: 19 ms (24.81%) | Memory: 36.5 MB (88.73%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -4,10 +4,14 @@ public:
         vector<vector<int>> v;
         sort(arr.begin(), arr.end());
         int mini = INT_MAX;
-        for(int i=1;i<arr.size();i++) mini = min(arr[i] - arr[i - 1], mini);
         for(int i=1;i<arr.size();i++){
             int diff = arr[i] - arr[i - 1];
-            if(diff == mini) v.push_back({arr[i - 1], arr[i]});
+            if(diff < mini){
+                mini = diff;
+                v.clear();
+                v.push_back({arr[i - 1], arr[i]});
+            }
+            else if(diff == mini) v.push_back({arr[i - 1], arr[i]});
         }
         return v;
     }
